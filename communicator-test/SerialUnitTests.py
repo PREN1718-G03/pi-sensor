e01ff492b8c63b6e4f456edf0c4c12bd59b7fab8
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-02-28 20:50:32 +0100

    Added Unit tests (test first... eh :D)

---
+++ communicator-test/SerialUnitTests.py
@@ -0,0 +1,50 @@
+from communicator.SerialObserver import SerialObserver
+from communicator.SerialListener import SerialListener
+import unittest
+
+
+class TestSerialListener(unittest.TestCase):
+    def setUp(self):
+        self.listener = SerialListener()
+        self.observer = TestObserver()
+
+    def testAttach(self):
+        self.listener.attach(self.observer)
+        test_result = False
+        for observer in self.listener.observers:
+            if observer == self.observer:
+                test_result = True
+        assert test_result
+
+    @unittest.expectedFailure
+    def testAttachFalse(self):
+        self.listener.attach(TestObserver())
+        test_result = False
+        for observer in self.listener.observers:
+            if observer == self.observer:
+                test_result = True
+        assert test_result
+
+    def testDetach(self):
+        self.listener.attach(self.observer)
+        test_result = True
+        self.listener.detach(self.observer)
+        for observer in self.listener.observers:
+            if observer == self.observer:
+                test_result = False
+        assert test_result
+
+    def tearDown(self):
+        pass
+
+
+class TestObserver(SerialObserver):
+    def __init__(self):
+        super(TestObserver, self).__init__()
+
+    def update(self, received_message):
+        print received_message
+
+
+if __name__ == '__main__':
+    unittest.main()
