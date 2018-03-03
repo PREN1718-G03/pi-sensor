68c28512992c61336640c7f9edf3da418aeb7896
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-03 16:43:08 +0100

    Python does not have method overloading Defaults are defined in the method header

--- camera_sensor/TargetModel.py
+++ camera_sensor/TargetModel.py
@@ -2,12 +2,8 @@ class TargetModel(object):
     """\
     Model for the target platform in PREN02 2018 HSLU
     """
-    def __init__(self):
-        self.target_found = False
-        self.polygons = []
-        self.distance = None
 
-    def __init__(self, target_found, polygons, distance):
+    def __init__(self, target_found=False, polygons=[], distance=None):
         """\
         Initialise the Target with attributes
 
@@ -16,16 +12,16 @@ class TargetModel(object):
             polygons List -- List with polygons determined to be the target platform
             distance Float -- Calculated distance from sensor to target platform
         """
-        self.__init__()
+
         if isinstance(target_found, bool):
             self.target_found = target_found
         else:
-            raise TypeError('target_found not of type boolean')
+            raise TypeError('target_found ' + str(type(target_found)) + ' not of type boolean')
         if isinstance(polygons, list):
             self.polygons = polygons
         else:
-            raise TypeError('polygons not of type list')
+            raise TypeError('polygons ' + str(type(polygons)) +' not of type list')
         if isinstance(distance,float):
             self.distance = distance
         else:
-            raise TypeError('distance not of type float')
+            self.distance = None
