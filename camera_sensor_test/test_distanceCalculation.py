c60f26a83dd6403a372803d96c09f989b23224bd
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-03 17:46:51 +0100

    Expanded DistanceCalculation tests

--- camera_sensor_test/test_distanceCalculation.py
+++ camera_sensor_test/test_distanceCalculation.py
@@ -1,6 +1,21 @@
-from unittest import TestCase
+from camera_sensor.DistanceCalculation import DistanceCalculation
+from camera_sensor.TargetModel import TargetModel
+import unittest
 
 
-class TestDistanceCalculation(TestCase):
-    def test_calculate_distance(self):
-        self.fail()
+class TestDistanceCalculation(unittest.TestCase):
+    def setUp(self):
+        self.distance_calculator = DistanceCalculation()
+
+    def testCalculateDistance(self):
+        target = TargetModel()
+        with self.assertRaises(NotImplementedError):
+            self.distance_calculator.calculate_distance(target)
+
+    def testCalculateDistanceBogusAttribute(self):
+        target = []
+        with self.assertRaises(TypeError):
+            self.distance_calculator.calculate_distance(target)
+
+    def tearDown(self):
+        pass
