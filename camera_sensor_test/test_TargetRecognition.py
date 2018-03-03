717c4bb05a5a708d9c28b2abdde6cd73fbdb9126
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-03 18:03:06 +0100

    Created test for TargetRecognition

---
+++ camera_sensor_test/test_TargetRecognition.py
@@ -0,0 +1,15 @@
+from camera_sensor.TargetRecognition import TargetRecognition
+from camera_sensor.TargetModel import TargetModel
+import unittest
+
+
+class TestDistanceCalculation(unittest.TestCase):
+    def setUp(self):
+        self.target_recogniser = TargetRecognition()
+
+    def testTargetRecognition(self):
+        target = self.target_recogniser.detect_target()
+        self.assertIsInstance(target, TargetModel)
+
+    def tearDown(self):
+        pass
