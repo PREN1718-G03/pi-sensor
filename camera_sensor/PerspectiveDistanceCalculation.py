4f096809a213ea7fafd15d5e17a2e85ed2b5e099
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-06 13:19:21 +0100

    Added diagnostic info output

--- camera_sensor/PerspectiveDistanceCalculation.py
+++ camera_sensor/PerspectiveDistanceCalculation.py
@@ -1,5 +1,6 @@
 from camera_sensor.DistanceCalculation import DistanceCalculation
 from camera_sensor.TargetModel import TargetModel
+import cv2
 
 
 class PerspectiveDistanceCalculation(DistanceCalculation):
@@ -9,5 +10,16 @@ class PerspectiveDistanceCalculation(DistanceCalculation):
     def calculate_distance(self, target):
         if not isinstance(target, TargetModel):
             raise TypeError('target not of type TargetModel')
+        else:
+            self._print_diagnostic_information(target)
+
+    def _print_diagnostic_information(self, target):
+        if isinstance(target, TargetModel):
+            i = 0
+            for contour in target.contours:
+                i += 1
+                print('Contour ' + str(i) + ': ' + str(contour))
+                print('Area ' + str(cv2.contourArea(contour)))
+                print('Sides ' + str(cv2.arcLength(contour)))
         else:
             pass
