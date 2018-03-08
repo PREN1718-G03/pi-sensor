cc2ca50ac870eff2347293890ced9980d97e8137
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-08 18:27:24 +0100

    Find the center of the TargetModel object

--- camera_sensor/PerspectiveDistanceCalculation.py
+++ camera_sensor/PerspectiveDistanceCalculation.py
@@ -11,7 +11,7 @@ class PerspectiveDistanceCalculation(DistanceCalculation):
         if not isinstance(target, TargetModel):
             raise TypeError('target not of type TargetModel')
         else:
-
+            self.target = target
             self._print_diagnostic_information(target)
 
     def _calculate_vertical_distance(self):
@@ -25,6 +25,37 @@ class PerspectiveDistanceCalculation(DistanceCalculation):
             pass
         else:
             raise TypeError('target of type None')
+
+    def _find_target_center(self):
+        target_center = None
+        if isinstance(self.target, TargetModel):
+            # calculate coordinates of specific contours
+            coordinate_array = []
+
+            for contour in self.target.contours:
+                moments = cv2.moments(contour)
+                cx = int(moments['m10'] / moments['m00'])
+                cy = int(moments['m01'] / moments['m00'])
+
+                coordinates = (cx, cy)
+                coordinate_array.append(coordinates)
+
+                # err_max: How near the two middle points should be in pixels
+                err_max = 4
+                match = 0
+
+                compared_coordinates = (0, 0)
+                for coordinates in coordinate_array:
+                    difference_x = abs(compared_coordinates[0] - coordinates[0])
+                    difference_y = abs(compared_coordinates[1] - coordinates[1])
+                    if difference_x < err_max and difference_y < err_max:
+                        match += 1
+                    else:
+                        match = 0
+                        compared_coordinates = coordinates
+                    if match > 3:
+                        target_center = compared_coordinates
+        return target_center
 
     def _print_diagnostic_information(self, target):
         if isinstance(target, TargetModel):
