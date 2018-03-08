0da55734e55222bc64b9168ed7963f517763ece9
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-08 11:12:10 +0100

    Update ContourRecognitionTest

--- camera_sensor_test/test_ContourTargetRecognition.py
+++ camera_sensor_test/test_ContourTargetRecognition.py
@@ -6,10 +6,12 @@ import unittest
 class TestDistanceCalculation(unittest.TestCase):
     def setUp(self):
         self.target_recogniser = ContourTargetRecognition()
+        self.target_recogniser.start()
 
     def testTargetRecognition(self):
         target = self.target_recogniser.detect_target()
         self.assertIsInstance(target, TargetModel)
 
     def tearDown(self):
+        self.target_recogniser.stop()
         pass
