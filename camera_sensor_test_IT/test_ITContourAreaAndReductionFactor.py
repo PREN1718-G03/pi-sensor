df449d8a45d5886aec7ffd286ab15b7a10d6cb21
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-23 08:09:09 +0100

    Rewritten Integration Test

--- camera_sensor_test_IT/test_ITContourAreaAndReductionFactor.py
+++ camera_sensor_test_IT/test_ITContourAreaAndReductionFactor.py
@@ -2,7 +2,8 @@ from camera_sensor.ContourTargetRecognition import ContourTargetRecognition
 from camera_sensor.PerspectiveDistanceCalculation import PerspectiveDistanceCalculation
 import cv2
 import os
-import datetime
+import time
+
 
 class ContourAreaAndReductionFactorTest(object):
     def __init__(self):
@@ -15,16 +16,17 @@ class ContourAreaAndReductionFactorTest(object):
         os.chdir('/home/pi/Desktop')
         bildnummer = 0
         while self.run:
-            user_input = raw_input("Press Enter...")
+            # user_input = raw_input("Press Enter...")
             bildnummer += 1
-            if str(user_input) == 'end':
-                self.run = False
-            else:
-                target = self.target_recogniser.detect_target()
-                self.distance_calculator.calculate_distance(target)
-                frame = self.target_recogniser.cam.read()
-                image_name = './imagePython' + str(bildnummer) + '.jpg'
-                cv2.imwrite(image_name, frame)
+            # if str(user_input) == 'end':
+            #     self.run = False
+            # else:
+            target = self.target_recogniser.detect_target()
+            self.distance_calculator.calculate_distance(target)
+            frame = self.target_recogniser.cam.read()
+            image_name = './image_testRun' + str(bildnummer) + '.jpg'
+            cv2.imwrite(image_name, frame)
+            time.sleep(0.5)
         self.end_test_and_cleanup()
 
     def end_test_and_cleanup(self):
