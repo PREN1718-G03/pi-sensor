7f3159cae35b6489fbad016ff56f10fc85f88437
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-23 11:03:25 +0100

    Wrote test for Factory

---
+++ communicator_test/test_SerialCommunicatorFactory.py
@@ -0,0 +1,17 @@
+import unittest
+from communicator.CommunicatorFactory import CommunicatorFactory
+from communicator.CommunicationInterfaceListener import CommunicationInterfaceListener
+from communicator.CommunicationInterfaceSender import CommunicationInterfaceSender
+
+
+class test_SerialCommunicatorFactory(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_communication_interface_sender(self):
+        sender = CommunicatorFactory.get_communication_interface_sender()
+        self.assertIsInstance(sender, CommunicationInterfaceSender)
+
+    def get_communication_interface_listener(self):
+        listener = CommunicatorFactory.get_communication_interface_listener()
+        self.assertIsInstance(listener, CommunicationInterfaceListener)
