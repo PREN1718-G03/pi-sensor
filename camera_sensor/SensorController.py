22a7afb0bd9eadd14d93f641e8ea6d664d6b5153
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-23 15:54:23 +0100

    Added docstring to SensorController

--- camera_sensor/SensorController.py
+++ camera_sensor/SensorController.py
@@ -6,4 +6,5 @@ class SensorController(object):
 
     @abc.abstractmethod
     def get_target_and_distance(self):
+    	"""Returns if target is found and the distance to the target in cm"""
         raise NotImplementedError('Must define get_target_and_distance to use this class')
