208acbed9c4d1de45d5cc2422cd603563969db3e
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-05 20:44:36 +0200

    Changed intergration test

--- communicator_test_IT/test_ArduinoSerialCommunication.py
+++ communicator_test_IT/test_ArduinoSerialCommunication.py
@@ -1,6 +1,8 @@
 from communicator.SerialCommunicationInterfaceListener import SerialCommunicationInterfaceListener
 from communicator.CommunicationInterfaceObserver import CommunicationInterfaceObserver
+from communicator.SerialCommunicationInterfaceSender import SerialCommunicationInterfaceSender
 import time
+
 
 class ArduinoSerialListener(CommunicationInterfaceObserver):
     def __init__(self):
@@ -13,15 +15,22 @@ class ArduinoSerialListener(CommunicationInterfaceObserver):
     def get_stored_message(self):
         return self.stored_message
 
+
 if __name__ == '__main__':
     time.sleep(2)
     serial_listener = SerialCommunicationInterfaceListener()
+    serial_sender = SerialCommunicationInterfaceSender()
     arduino_listener = ArduinoSerialListener()
     serial_listener.attach(arduino_listener)
     serial_listener.start()
     not_stop = True
+    index = 1
     while not_stop:
+        string_message = "Test " + str(index)
+        print "Sending " + string_message
+        serial_sender.send_message(string_message)
         user_input = raw_input("Press Enter...")
         if user_input == "end":
             not_stop = False
+        index += 1
     serial_listener.stop()
