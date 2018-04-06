ec9747d7e84aae0b27fe7812638f9dc976165d64
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-06 09:42:30 +0200

    Begun on SensorControllerImplementation

--- camera_sensor/SensorControllerImplementation.py
+++ camera_sensor/SensorControllerImplementation.py
@@ -1,6 +1,22 @@
 from SensorController import SensorController
+from ContourTargetRecognition import ContourTargetRecognition
+from PerspectiveDistanceCalculation import PerspectiveDistanceCalculation
+from TargetModel import TargetModel
 
 
 class SensorControllerImplementation(SensorController):
+    def __init__(self):
+        self.__target_recognition = ContourTargetRecognition()
+        self.__distance_calculation = PerspectiveDistanceCalculation()
+        self.__target_recognition.start()
+
     def get_target_and_distance(self):
-        pass
+        target = self.__target_recognition.detect_target()
+        distance = None
+        if isinstance(target, TargetModel):
+            if target.target_found:
+                distance = self.__distance_calculation.calculate_distance(target)
+            return target.target_found, distance
+
+    def close(self):
+        self.__target_recognition.stop()
