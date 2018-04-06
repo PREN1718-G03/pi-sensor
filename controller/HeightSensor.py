5c0b08d726fb3c49654b4c3b8ac901610e53a721
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-06 10:50:38 +0200

    Added GPIO Setup

--- controller/HeightSensor.py
+++ controller/HeightSensor.py
@@ -1,6 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+
 class HeightSensor(object):
     def __init__(self):
-        pass
+        # GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+
+        # define GPIO pins
+        self.__GPIO_TRIGGER = 23
+        self.__GPIO_ECHO = 24
+
+        GPIO.cleanup()
+        # define direction of GPIO pins(IN / OUT)
+        GPIO.setup(self.__GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.__GPIO_ECHO, GPIO.IN)
 
     def get_height(self):
         height = 0.0
