41760eb522eef2089fe1850331ff96b62c2749c8
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-07 18:06:34 +0200

    PEP8 style thing -.-

--- controller_test_IT/test_HeightSensor.py
+++ controller_test_IT/test_HeightSensor.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from controller.HeightSensor import HeightSensor
 
+
 class TestHeightSensor(TestCase):
     def test_get_height(self):
         sensor = HeightSensor()
