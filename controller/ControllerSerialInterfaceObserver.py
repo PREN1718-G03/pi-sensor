76c5ed980d8c09d875dbd6cf72e4da5f28c8dc68
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-13 10:52:50 +0200

    Removed SerialInterfaceObserver implementation it was stupid

--- controller/ControllerSerialInterfaceObserver.py
+++
@@ -1,8 +0,0 @@
-from communicator.CommunicationInterfaceObserver import CommunicationInterfaceObserver
-
-class ControllerSerialInterfaceObserver(CommunicationInterfaceObserver):
-    def update(self, received_message):
-        print received_message
-
-    def __init__(self):
-        pass
--- controller/PiController.py
+++ controller/PiController.py
@@ -1,16 +1,24 @@
-from controller.ControllerSerialInterfaceObserver import ControllerSerialInterfaceObserver
+from communicator.CommunicationInterfaceObserver import CommunicationInterfaceObserver
 from communicator.CommunicatorFactory import CommunicatorFactory
 from communicator.CommunicationInterfaceListener import CommunicationInterfaceListener
 from communicator.CommunicationInterfaceSender import CommunicationInterfaceSender
+from camera_sensor.CameraSensorFactory import CameraSensorFactory
+from camera_sensor.SensorController import SensorController
 
 
-class PiController(object):
+class PiController(CommunicationInterfaceObserver):
+    def update(self, received_message):
+        print received_message
+
     def __init__(self):
-        communication_interface_observer = ControllerSerialInterfaceObserver()
-        communication_interface_listener = CommunicatorFactory.get_communication_interface_listener()
-        communication_interface_sender = CommunicatorFactory.get_communication_interface_sender()
+        self.__communication_interface_listener = CommunicatorFactory.get_communication_interface_listener()
+        self.__communication_interface_sender = CommunicatorFactory.get_communication_interface_sender()
 
+        self.__camera_sensor_factory = CameraSensorFactory()
+        self.__camera_sensor = CameraSensorFactory.get_sensor_controller()
 
+        if isinstance(self.__communication_interface_listener, CommunicationInterfaceListener):
+            self.__communication_interface_listener.attach(self)
 
 if __name__ == '__main__':
     controller = PiController()
