fb866ef8ba4dec8f1ccb9b005d679747802a6ade
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-13 11:03:06 +0200

    Completed PiController setup and cleanup

--- controller/PiController.py
+++ controller/PiController.py
@@ -4,9 +4,11 @@ from communicator.CommunicationInterfaceListener import CommunicationInterfaceLi
 from communicator.CommunicationInterfaceSender import CommunicationInterfaceSender
 from camera_sensor.CameraSensorFactory import CameraSensorFactory
 from camera_sensor.SensorController import SensorController
-
+from controller.HeightSensor import HeightSensor
+import warnings
 
 class PiController(CommunicationInterfaceObserver):
+    # Implementation of SerialInterfaceObserver methods
     def update(self, received_message):
         print received_message
 
@@ -17,8 +19,22 @@ class PiController(CommunicationInterfaceObserver):
         self.__camera_sensor_factory = CameraSensorFactory()
         self.__camera_sensor = CameraSensorFactory.get_sensor_controller()
 
+        self.__height_sensor = HeightSensor()
+
         if isinstance(self.__communication_interface_listener, CommunicationInterfaceListener):
+            self.__communication_interface_listener.start()
             self.__communication_interface_listener.attach(self)
+        else:
+            warnings.warn("Listener not started", RuntimeWarning)
+
+        if not isinstance(self.__communication_interface_sender, CommunicationInterfaceSender):
+            warnings.warn("Sender is not subtype of CommunicationInterfaceSender")
+
+    def close(self):
+        self.__communication_interface_listener.stop()
+        self.__height_sensor.close()
+        self.__camera_sensor.close()
 
 if __name__ == '__main__':
     controller = PiController()
+    controller.close()
