936530430655544bdf830cb46bb3ed62d5bd551e
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-13 10:43:11 +0200

    Outsourced the SerialInterfaceObserver implementation in own class

--- controller/PiController.py
+++ controller/PiController.py
@@ -1,6 +1,15 @@
+from controller.ControllerSerialInterfaceObserver import ControllerSerialInterfaceObserver
+from communicator.CommunicatorFactory import CommunicatorFactory
+from communicator.CommunicationInterfaceListener import CommunicationInterfaceListener
+from communicator.CommunicationInterfaceSender import CommunicationInterfaceSender
+
+
 class PiController(object):
     def __init__(self):
-        pass
+        communication_interface_observer = ControllerSerialInterfaceObserver()
+        communication_interface_listener = CommunicatorFactory.get_communication_interface_listener()
+        communication_interface_sender = CommunicatorFactory.get_communication_interface_sender()
+
 
 
 if __name__ == '__main__':
