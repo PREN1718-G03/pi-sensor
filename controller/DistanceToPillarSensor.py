3d5fcd3ed365aff9d045de8bba554027938c22ef
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-20 10:04:44 +0200

    Refactor copied the height_sensor

---
+++ controller/DistanceToPillarSensor.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class DistanceToPillarSensor(object):
+    def __init__(self):
+        # GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+
+        # define GPIO pins
+        self.__GPIO_TRIGGER = 17
+        self.__GPIO_ECHO = 27
+
+        # define direction of GPIO pins(IN / OUT)
+        GPIO.setup(self.__GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.__GPIO_ECHO, GPIO.IN)
+
+        # TODO Solve RuntimeWarning: This Channel already in use
+        GPIO.setwarnings(False)
+
+    def get_distance(self):
+        height = 0.0
+        GPIO.output(self.__GPIO_TRIGGER, True)
+
+        # set trigger to LOW after 0.00001 seconds
+        time.sleep(0.00001)
+        GPIO.output(self.__GPIO_TRIGGER, False)
+
+        time_start = time.time()
+        time_stop = time.time()
+
+        while GPIO.input(self.__GPIO_ECHO) == 0:
+            time_start = time.time()
+
+        while GPIO.input(self.__GPIO_ECHO) == 1:
+            time_stop = time.time()
+
+        time_elapsed = time_stop - time_start
+        distance = (time_elapsed * 34300) / 2
+
+        time.sleep(0.05)
+        return distance
+
+    def close(self):
+        GPIO.cleanup()
