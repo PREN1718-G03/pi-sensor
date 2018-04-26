e081dea16f8fea2356dcbba297fe4397dccddd3b
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-26 17:04:37 +0200

    Implemented polling method for the ET board

--- controller/PiController.py
+++ controller/PiController.py
@@ -12,12 +12,33 @@ class PiController(CommunicationInterfaceObserver):
     # Implementation of SerialInterfaceObserver methods
     def update(self, received_message):
         if received_message == "SendData":
-            pass
+            print "Received request for data from ET board"
+            data_to_send = ''
+            usb, lsb = self.__transform_to_char_ints(self.__distance)
+            data_to_send = usb + lsb
+            usb, lsb = self.__transform_to_char_ints(self.__height)
+            data_to_send = data_to_send + usb + lsb
+            if self.__target_recognised:
+                usb, lsb = self.__transform_to_char_ints(self.__distance_to_target)
+                data_to_send = data_to_send + usb + lsb
+            else:
+                data_to_send = data_to_send + chr(255) + chr(255)
+            print data_to_send
+            self.__communication_interface_sender.send_message(data_to_send)
+
+    def __transform_to_char_ints(self, value):
+        result_value = int(round(value))
+        integer_as_bitstring = '{0:016b}'.format(result_value)
+        upper_significant_byte_as_char = chr(int(integer_as_bitstring[:8], 2))
+        lower_significant_byte_as_char = chr(int(integer_as_bitstring[8:], 2))
+        return upper_significant_byte_as_char, lower_significant_byte_as_char
 
     def __init__(self):
         self.not_stopped = True
-        self.__height = 0;
-        self.__distance = 0;
+        self.__height = 0
+        self.__distance = 0
+        self.__target_recognised = False
+        self.__distance_to_target = 0
 
         self.__communication_interface_listener = CommunicatorFactory.get_communication_interface_listener()
         self.__communication_interface_sender = CommunicatorFactory.get_communication_interface_sender()
@@ -50,9 +71,9 @@ class PiController(CommunicationInterfaceObserver):
         self.__distance = self.__distance_sensor.get_distance()
 
         self.__camera_sensor.set_height(self.__height)
-        target_recognized, distance = self.__camera_sensor.get_target_and_distance()
+        self.__target_recognised, distance = self.__camera_sensor.get_target_and_distance()
 
-        if target_recognized:
+        if self.__target_recognised:
             print "Target recognized in a distance of " + str(distance) + " cm"
         else:
             print "Target not recognized"
