56a74564dc6cbe8bb014b6eb81a951395feb62c0
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-26 11:14:31 +0200

    Added distance sensor to controller

--- controller/PiController.py
+++ controller/PiController.py
@@ -3,16 +3,21 @@ from communicator.CommunicatorFactory import CommunicatorFactory
 from communicator.CommunicationInterfaceListener import CommunicationInterfaceListener
 from communicator.CommunicationInterfaceSender import CommunicationInterfaceSender
 from camera_sensor.CameraSensorFactory import CameraSensorFactory
+from controller.DistanceToPillarSensor import DistanceToPillarSensor
 from controller.HeightSensor import HeightSensor
 import warnings
+
 
 class PiController(CommunicationInterfaceObserver):
     # Implementation of SerialInterfaceObserver methods
     def update(self, received_message):
-        print received_message
+        if received_message == "SendData":
+            pass
 
     def __init__(self):
         self.not_stopped = True
+        self.__height = 0;
+        self.__distance = 0;
 
         self.__communication_interface_listener = CommunicatorFactory.get_communication_interface_listener()
         self.__communication_interface_sender = CommunicatorFactory.get_communication_interface_sender()
@@ -21,6 +26,7 @@ class PiController(CommunicationInterfaceObserver):
         self.__camera_sensor = CameraSensorFactory.get_sensor_controller()
 
         self.__height_sensor = HeightSensor()
+        self.__distance_sensor = DistanceToPillarSensor()
 
         if isinstance(self.__communication_interface_listener, CommunicationInterfaceListener):
             print "Setting up listener"
@@ -36,22 +42,27 @@ class PiController(CommunicationInterfaceObserver):
         print "Cleaning up PiController object"
         self.__communication_interface_listener.stop()
         self.__height_sensor.close()
+        self.__distance_sensor.close()
         self.__camera_sensor.close()
 
     def control(self):
-        height = self.__height_sensor.get_height()
-        self.__camera_sensor.set_height(height)
+        self.__height = self.__height_sensor.get_height()
+        self.__distance = self.__distance_sensor.get_distance()
+
+        self.__camera_sensor.set_height(self.__height)
         target_recognized, distance = self.__camera_sensor.get_target_and_distance()
+
         if target_recognized:
             print "Target recognized in a distance of " + str(distance) + " cm"
         else:
             print "Target not recognized"
 
+
 if __name__ == '__main__':
     controller = PiController()
     while controller.not_stopped:
         controller.control()
-        #TODO remove raw input and provide graceful shutdown possibility
+        # TODO remove raw input and provide graceful shutdown possibility
         user_input = raw_input("Press Enter...")
         if user_input == "end":
             controller.not_stopped = False
